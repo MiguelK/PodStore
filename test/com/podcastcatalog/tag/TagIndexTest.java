3bc4ba6218b1a0bc7257f1fc1f205690cbce620c
Miguel Krantz <miguel.krantz@telenor.se>
2019-08-28 12:43:19 +0200

    Open App Webb...

--- test/com/podcastcatalog/tag/TagIndexTest.java
+++ test/com/podcastcatalog/tag/TagIndexTest.java
@@ -1,28 +1,16 @@
-package com;
+package com.podcastcatalog.tag;
 
-import com.podcastcatalog.TestUtil;
-import com.podcastcatalog.model.PodCastCatalogMetaData;
 import com.podcastcatalog.model.podcastcatalog.PodCast;
-import com.podcastcatalog.model.podcastcatalog.PodCastEpisode;
 import com.podcastcatalog.modelbuilder.collector.itunes.ItunesSearchAPI;
-import com.podcastcatalog.tag.TagManager;
-import com.podcastcatalog.util.IOUtil;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.util.List;
 
-/**
- * Created by miguelkrantz on 2019-04-24.
- */
 public class TagIndexTest {
-
-
 
     @Test
     public void parseName() throws Exception {
         TagManager.getInstance().createTagIndex("SWE");
-
         TagManager.getInstance().indexContent("zlatan ibrahimovic");
     }
 
@@ -31,9 +19,8 @@ public class TagIndexTest {
 
         TagManager.getInstance().createTagIndex("SWE");
 
-        ItunesSearchAPI query = ItunesSearchAPI.createCollector("term=P3&entity=podcast&limit=1&country=SE");
+        ItunesSearchAPI query = ItunesSearchAPI.createCollector("term=Spår&entity=podcast&limit=1&country=SE");
         List<PodCast> podCasts = query.collectPodCasts();
-
         PodCast podCast1 = podCasts.get(0);
 
         String title = podCast1.getTitle();
