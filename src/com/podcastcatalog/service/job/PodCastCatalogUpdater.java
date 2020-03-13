9fcf1c54bd157223a190a9cf3d3826746a3f0d14
Miguel Krantz <miguel.krantz@telenor.se>
2020-03-13 16:04:10 +0100

    Removed info logs

--- src/com/podcastcatalog/service/job/PodCastCatalogUpdater.java
+++ src/com/podcastcatalog/service/job/PodCastCatalogUpdater.java
@@ -28,9 +28,9 @@ public class PodCastCatalogUpdater implements Job {
             for (PodCastCatalogLanguage language : PodCastCatalogLanguage.values()) {
 
                 //TEST only 1
-               if (language != PodCastCatalogLanguage.US) {
-                     continue;
-                }
+                //if (language != PodCastCatalogLanguage.US) {
+                //      continue;
+                //}
 
                 /*if(PodCastCatalogService.getInstance().isMetaDataRegistered(language)) {
                     LOG.info("PodCastCatalogMetaData already exist, will update for lang=" + language);
